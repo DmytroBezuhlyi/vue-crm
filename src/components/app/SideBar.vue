981563fad8bf39c5e6a4bc5aa551d9affc16383d
Dmytro Bezuhlyi | CHISW JS Dev <dmytro.bezuhlyi@chisw,com>
2021-10-13 13:48:48 +0300

    added dynamic links in the component

---
+++ src/components/app/SideBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul class="sidenav app-sidenav open">
+    <router-link
+        tag="li"
+        active-class="active"
+        v-for="link in links"
+        :key="link.url"
+        :to="link.url"
+        :exact="link.exact"
+    >
+      <a href="#" class="waves-orange pointer">{{ link.title }}</a>
+    </router-link>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "SideBar",
+  data: () => ({
+    links: [
+      {title: 'Invoice', url: '/', exact: true},
+      {title: 'History', url: '/history'},
+      {title: 'Planning', url: '/planning'},
+      {title: 'New Record', url: '/record'},
+      {title: 'Categories', url: '/categories'},
+    ],
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
